cb862c2d3c8350fcc7248937a3710a53d558fb43
antoinemeyer5 <antoine.meyer54@gmail.com>
2025-08-18 09:50:43 +0200

    feat: add invoice h

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,10 @@
 
 <div class="flex width-65vw ml-mr mt-5" style="justify-content: space-between;">
 	<h1 class="no_print">FacturePro</h1>
+	<div>
+		<a class="no_print" href="/devis_1"><button class="button">Devis (m²)</button></a>
+		<a class="no_print" href="/facture_1"><button class="button">Facture (h)</button></a>
+	</div>
 	<button class="no_print button" onclick={print}> Téléchargez le PDF </button>
 </div>
 
